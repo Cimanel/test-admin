7ad4e9e95385114c0a768834ee232e95134a2b73
Cimanel <cindy@marmelab.com>
2022-04-07 23:04:35 +0200

    rm console

--- src/data/DataProvider.tsx
+++ src/data/DataProvider.tsx
@@ -2,6 +2,5 @@ import generateData from "data-generator-retail";
 import fakeDataProvider from "ra-data-fakerest";
 
 const data = generateData();
-console.log(data);
 
 export const dataProvider = fakeDataProvider(data);
